f2be9a319b5ac0606c27b11323441999552031e1
zhandu <80059972+zhandu@users.noreply.github.com>
2022-03-04 16:23:23 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
-const DB_USER = 'tidnvb'
-const DB_PASSWORD = 'zi8e6--p1'
-const DB_NAME = 'qcm'
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
 
 module.exports = {
     DB_URI: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xmhlt.mongodb.net/${DB_NAME}`
